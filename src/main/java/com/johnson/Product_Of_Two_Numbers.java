b8b72caf469b371f5cd7f76fac07e25a9daee289
rittik-hub <71030560+rittik-hub@users.noreply.github.com>
2020-10-02 12:29:10 +0530

    Update Product_Of_Two_Numbers.java

--- src/main/java/com/johnson/Product_Of_Two_Numbers.java
+++ src/main/java/com/johnson/Product_Of_Two_Numbers.java
@@ -2,9 +2,11 @@ package com.johnson;
 
 import java.util.Scanner;
 
-public class Product_Of_Two_Numbers {
+public class Product_Of_Two_Numbers
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         int num1, num2;
 
@@ -13,8 +15,6 @@ public class Product_Of_Two_Numbers {
         num1 = num.nextInt();
         System.out.println("Enter second number:");
         num2 = num.nextInt();
-
-        int result = num1 * num2;
-        System.out.println(num1 + " x " + num2 + " = " + result);
+        System.out.println(num1 + " x " + num2 + " = " + (num1*num2));
     }
 }
